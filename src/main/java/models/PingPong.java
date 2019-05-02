ecdc7ae53d15c604a8219c72f1cc1510584645dc
victor njonge <vinge1718@gmail.com>
2019-05-02 12:32:51 +0300

    added the test files for backend tests

--- src/main/java/models/PingPong.java
+++ src/main/java/models/PingPong.java
@@ -1,4 +1,12 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PingPong {
+
+    public ArrayList<Object> runPingPong(int countUpTo){
+        ArrayList<Object> result = new ArrayList<Object>();
+        return null;
+    }
 }
---
+++ src/test/java/models/PingPongTest.java
@@ -0,0 +1,16 @@
+package models;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PingPongTest {
+    @Test
+    public void runPingPong_countUpToOne_ArrayList() {
+        PingPong testPingPong = new PingPong();
+        List<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        assertEquals(expectedOutput, testPingPong.runPingPong(1));
+    }
+}
