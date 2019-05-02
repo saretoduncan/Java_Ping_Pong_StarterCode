f96dd6e84ca962cac265ad1a9ac86e59ba332fe2
victor njonge <vinge1718@gmail.com>
2019-05-02 12:43:05 +0300

    all the red green tests and their repsective functional back ends

--- src/main/java/models/PingPong.java
+++ src/main/java/models/PingPong.java
@@ -8,7 +8,15 @@ public class PingPong {
     public ArrayList<Object> runPingPong(int countUpTo){
         ArrayList<Object> result = new ArrayList<Object>();
         for (int i = 1; i <= countUpTo; i++){
-            result.add(i);
+            if (i % 3 == 0 && i % 5 == 0){
+                result.add("pingpong");
+            } else if (i % 3 == 0){
+                result.add("ping");
+            } else if (i % 5 == 0){
+                result.add("pong");
+            } else {
+                result.add(i);
+            }
         }
         return result;
     }
